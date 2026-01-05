7bfa14632014374d426c34dcccb562b8806239aa
yemanealem <yemanea223@gmail.com>
2026-01-05 19:19:30 +0100

    strategy pattern payment

---
+++ Behavioral/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        PaymentContext context = new PaymentContext();
+
+        context.setPaymentStrategy(new CreditCardPayment());
+        context.executePayment(100);
+
+        context.setPaymentStrategy(new PayPalPayment());
+        context.executePayment(200);
+
+        context.setPaymentStrategy(new BankTransferPayment());
+        context.executePayment(300);
+    }
+}
---
+++ Behavioral/PaymentContext.java
@@ -0,0 +1,12 @@
+public class PaymentContext {
+
+    private PaymentStrategy paymentStrategy;
+
+    public void setPaymentStrategy(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void executePayment(double amount) {
+        paymentStrategy.pay(amount);
+    }
+}
